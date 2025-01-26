f26344394634b5610cbb592bacfc35df19e331f3
hellosiyam <hellosiyambd@gmail.com>
2025-01-26 19:37:59 +0600

    fetch

---
+++ js/apps.js
@@ -0,0 +1,15 @@
+document.getElementById('btn2').addEventListener('click', function () {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(res => res.json())
+    .then(data => console.log(data))
+});
+
+function data() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data1 => data3(data1))
+}
+
+function data3(data4) {
+    console.log(data4);
+}
